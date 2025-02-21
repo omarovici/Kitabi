d070791ff908a5d3030a6d90870c71b5ef5b5236
omarovici <omarkhalidsaber042004@gmail.com>
2025-02-21 23:20:10 +0200

    Create Project

---
+++ Kitabi/Kitabi.Presentation.Layer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Kitabi.Presentation.Layer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
